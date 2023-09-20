c9dd428856d07447c6892f9bfa14af829fba90f1
Levy <levymcruz@gmail.com>
2023-09-20 15:12:41 -0300

    finishing ex 05

---
+++ aula-05/prisma/migrations/20230920180911_migration_01/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "label" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "expirationDate" TIMESTAMP(3) NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "eatable" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
